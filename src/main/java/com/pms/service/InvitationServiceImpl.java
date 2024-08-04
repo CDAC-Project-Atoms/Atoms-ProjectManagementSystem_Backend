b6ad339b4f7fb92d20609a074cd57a6eca46fd43
Rushikesh Patil <rushipatilrp07@gmail.com>
2024-08-04 22:26:45 +0530

    Update InvitationServiceImplimentation
    Update InvitationServiceImplimentation

--- src/main/java/com/pms/service/InvitationServiceImpl.java
+++ src/main/java/com/pms/service/InvitationServiceImpl.java
@@ -2,13 +2,17 @@ package com.pms.service;
 
 import javax.mail.MessagingException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pms.model.Invitation;
+import com.pms.repository.InvitationRepository;
 
 @Service
 public class InvitationServiceImpl implements InvitationService{
 
+	@Autowired
+	private InvitationRepository invitationRepository;
 	
 	@Override
 	public void sendInvitation(String email, Long projectId) throws MessagingException {
