22874541568df0ff49754635e25291339ebc62ed
Pushpak Yele <158846404+Pushpak-Project@users.noreply.github.com>
2024-08-04 20:23:18 +0530

    Changes in Issue and Project Model
    Added ElementCollection annotation in tag field.
    
    In application properties - added spring datasource as mysql

--- src/main/java/com/pms/model/Project.java
+++ src/main/java/com/pms/model/Project.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -33,6 +34,7 @@ public class Project {
 	private String description;
 	private String category;
 	
+	@ElementCollection
 	private List<String> tags = new ArrayList<>();
 	
 	@JsonIgnore
